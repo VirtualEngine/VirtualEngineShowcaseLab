44e9a49f520907caa20c85f6bf3530f2dfe5c991
Iain Brighton <iainbrighton@hotmail.com>
2016-07-20 14:31:53 +0100

    Adds TerminalServerLicenseServers group membership

--- DSCResources/vShowcaseLab/vShowcaseLab.schema.psm1
+++ DSCResources/vShowcaseLab/vShowcaseLab.schema.psm1
@@ -64,7 +64,11 @@ configuration vShowcaseLab {
 
         ## Directory path containing user thumbnail photos
         [Parameter()] [ValidateNotNullOrEmpty()]
-        [System.String] $ThumbnailPhotoPath
+        [System.String] $ThumbnailPhotoPath,
+
+        ## Members to add to the 'Terminal Server License Servers' group
+        [Parameter()] [ValidateNotNullOrEmpty()]
+        [System.String[]] $TerminalServerLicenseServers = 'CONTROLLER$'
     )
 
     ## Avoid recursive loading of the VirtualEngineTrainingLab composite resource
@@ -261,8 +265,10 @@ configuration vShowcaseLab {
             @{ Name = 'RES WM Service Accounts'; Path = 'OU=Groups,OU=Showcase'; Description = 'RES ONE Workspace service accounts';
                     Members = 'Domain Admins','RESWM'; Scope = 'DomainLocal'; }
 
-            ## Add RES AM Service Account to domain admins
+            ## Add RES AM Service Account to 'Domain Admins' group
             @{ Name = 'Domain Admins'; Path = 'CN=Users'; Members = 'RESAM'; }
+            ## Add CONTROLLER to 'Terminal Server License Servers' group
+            @{ Name = 'Terminal Server License Servers'; Path = 'CN=Builtin'; Members = $TerminalServerLicenseServers; }
         )
 
     } #end ActiveDirectory
